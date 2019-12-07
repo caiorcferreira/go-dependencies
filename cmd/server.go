8ee7adf23dc5ffc9ff1acfa859e205c91b7d3502
Caio Ferreira <caio.cavalcante@b2wdigital.com>
2019-12-07 14:48:26 -0300

    refact: change port to enviroment variable

--- cmd/server.go
+++ cmd/server.go
@@ -4,13 +4,19 @@ import (
 	"fmt"
 	"github.com/caiorcferreira/swapi/internals/swapi/api"
 	"net/http"
+	"os"
 )
 
-func StartServer(port int) error {
+func StartServer() error {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
 	router := api.SetupRoutes()
 
 	s := &http.Server{
-		Addr: fmt.Sprintf(":%d", port),
+		Addr: fmt.Sprintf(":%s", port),
 		Handler:router,
 	}
 
